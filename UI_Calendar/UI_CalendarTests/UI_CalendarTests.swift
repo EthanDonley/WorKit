be6d5e1f08ac76ded5ccfab65c1243f6888ecc53
Miguel Constantino-Guzman <114381178+MConstantinoGuzman@users.noreply.github.com>
2024-11-14 13:57:39 -0800

    Added UI Calendar
    This calendar takes into account the arrow buttons, the current date, and other formats for the user to understand. 
    
    Future: incorporate a button to press on a date.

---
+++ UI_Calendar/UI_CalendarTests/UI_CalendarTests.swift
@@ -0,0 +1,17 @@
+//
+//  UI_CalendarTests.swift
+//  UI_CalendarTests
+//
+//  Created by Miguel Guzman on 11/13/24.
+//
+
+import Testing
+@testable import UI_Calendar
+
+struct UI_CalendarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
